ca2ef9cd9dc01386efe397f92b24c66298d4bb9f
secretsparkle <scott.madera@gmail.com>
2019-02-11 21:46:42 -0800

    Implemented the basic moveTree build. Currently able to build to 7 levels before timing out.

--- DeepConnect.java
+++ DeepConnect.java
@@ -11,18 +11,43 @@ import java.util.ArrayList;
  * the terminate flag.  However, your AI needs to check for the terminate flag.
  *
  * @author Scott Madera
+ * (add your name here)
  */
-public class DeepConnect extends AIModule
-{
-	public void getNextMove(final GameStateModule game)
-	{
-		for(int i = 0; i < game.getWidth(); i++)
-			if(game.canMakeMove(i))
-			{
-				chosenMove = i;
-				break;
-			}
-	}
+public class DeepConnect extends AIModule {
+    public void getNextMove(final GameStateModule game) {
+        Node tree = new Node(game);
+        buildTree(tree, 7);
+
+        for(int i = 0; i < game.getWidth(); i++) {
+            if(game.canMakeMove(i)) {
+                chosenMove = i;
+                break;
+            }
+        }
+    }
+
+    public Node buildTree(Node tree, int levels) {
+        if (levels == 0) {
+            return tree;
+        }
+        GameStateModule stateCopy;
+        for (int i = 0; i < tree.getState().getWidth(); i++) {
+            stateCopy = tree.getState().copy();
+            try {
+                stateCopy.makeMove(i);
+                Node newChild = new Node(stateCopy);
+                tree.addChild(newChild);
+            }
+            catch (Exception e) {
+                //Do something fun
+                ;
+            }
+        }
+        for (int i = 0; i < tree.getChildren().size(); i++) {
+            tree.getChildren().set(i, buildTree(tree.getChildren().get(i), levels-1));
+        }
+        return tree;
+    }
 }
 
 class Node {
