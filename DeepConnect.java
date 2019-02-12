38101e4f481cfda26ac792d833b1cf445f77367f
secretsparkle <scott.madera@gmail.com>
2019-02-11 21:45:04 -0800

    Fleshed out the Node class.

--- DeepConnect.java
+++ DeepConnect.java
@@ -25,17 +25,48 @@ public class DeepConnect extends AIModule
 	}
 }
 
-public class Node {
-    private Int score;
+class Node {
+    private Integer score;
     private GameStateModule state;
     private ArrayList<Node> children;
 
-    public Bool isLeafNode() {
-        if (children.length() == 0) {
+    Node() {
+        score = 0;
+        children = new ArrayList<Node>();
+    }
+    Node(final GameStateModule newState) {
+        score = 0;
+        state = newState;
+        children = new ArrayList<Node>();
+    }
+
+    public void addChild(Node child) {
+        children.add(child);
+    }
+
+    public ArrayList<Node> getChildren() {
+        return children;
+    }
+    public Integer getScore() {
+        return this.score;
+    }
+    public GameStateModule getState() {
+        return state;
+    }
+
+    public Boolean isLeafNode() {
+        if (children.size() == 0) {
             return true;
         }
         else {
             return false;
         }
+    }
+
+    public void setScore(int scoreIn) {
+        this.score = scoreIn;
+    }
+    public void setState(GameStateModule stateIn) {
+        this.state = stateIn;
     }
 }
