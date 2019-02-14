133713e14465908f50abf6ce72ae785bc6a8fce7
secretsparkle <scott.madera@gmail.com>
2019-02-14 15:53:40 -0800

    Updated Tree class.

--- DeepConnect.java
+++ DeepConnect.java
@@ -378,13 +378,17 @@ class Tree {
 
     Tree(Node newRoot) {
         root = newRoot;
-        leafs = new ArrayList<Node>();
+        leaves = new ArrayList<Node>();
     }
 
     //If we store leaves, do we need a DFS?
     //How should we gather the leaves?
     public void DFS() {
-        ;
+        for (Node child : root.getChildren()) {
+            if (child.isLeafNode()) {
+                
+            }
+        }
     }
 }
 
