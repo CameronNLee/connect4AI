4013dd301b10a37339a70c7cc6670bda03172a2e
cmnlee <cmnlee@ucdavis.edu>
2019-02-13 19:29:33 -0800

    Added payoffs for when the leaf is already determined to be a Game Over board state.

--- DeepConnect.java
+++ DeepConnect.java
@@ -116,8 +116,29 @@ public class DeepConnect extends AIModule {
 
     // based on a leaf node's board state, determine the payoff
     public int calculatePayoff(Node leaf) {
-        /* do stuff here */
-        return 0; // placeholder
+
+        // case 1: leaf contains a board state who's game is over.
+        // So, determine who the winner is, and assign payoffs based on that.
+        if (leaf.getState().isGameOver()) {
+            if (leaf.getState().getWinner() == player) {
+                return 1;
+            }
+            else if (leaf.getState().getWinner() == enemy){
+                return -1; // enemy won, so discourage taking this path!
+            }
+            else {
+                return 0; // draw
+            }
+        }
+
+        // case 2: game isn't over yet. Predict what the best payoffs are
+        // based on how many potential 4-in-a-rows could exist for player,
+        // versus how many potential 4-in-a-rows could exist for enemy.
+
+        // if leaf's board state has it so that there are more 4-in-a-rows for enemy
+        // than there are 4-in-a-rows for player, then assign negative payoff.
+        return determineIfWinningOrLosingState(leaf);
+
     }
 
     public int determineIfWinningOrLosingState(Node leaf) {
@@ -139,9 +160,9 @@ public class DeepConnect extends AIModule {
         int enemyStreak = 0;
         int occupies = 0;
 
-        for (int i = 0; i < leaf.getState().getHeight(); i++) {
-            for (int j = 0; j < leaf.getState().getWidth(); j++) {
-                occupies = leaf.getState().getAt(j,i);
+        for (int row = 0; row < leaf.getState().getHeight(); row++) {
+            for (int col = 0; col < leaf.getState().getWidth(); col++) {
+                occupies = leaf.getState().getAt(col, row);
                 if (occupies == player) {
                     playerStreak += 1;
                     enemyStreak = 0;
